5217f507d9ebf26f9d64f82041935439cb5a9116
Pedro Costa <pedrocostaugusto@gmail.com>
2022-08-05 19:21:40 -0300

    Correção do nome da página do jogo

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Settings from './pages/Settings';
-import GamePage from './pages/GamePage';
+import Game from './pages/Game';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
       <Route path="/settings" component={ Settings } />
-      <Route path="/gamepage" component={ GamePage } />
+      <Route path="/gamepage" component={ Game } />
     </Switch>
   );
 }
--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class GamePage extends React.Component {
+export default class Game extends React.Component {
   render() {
     return (
       <div>GamePage</div>
