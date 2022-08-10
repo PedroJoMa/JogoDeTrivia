07ad294f83c17ff46cb7441e019113f56cd9df8a
Matheus Santos <matheus.p.1996@hotmail.com>
2022-08-10 16:09:22 -0300

    Requisito 18 completo

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends React.Component {
+  goHome = () => {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     return (
-      <div data-testid="ranking-title">Ranking</div>
+      <div data-testid="ranking-title">
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.goHome }
+        >
+          home
+        </button>
+      </div>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.object,
+}.isRequired;
 
 export default Ranking;
